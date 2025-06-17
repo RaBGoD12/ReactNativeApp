e02ae79d80ace58fa8042c3d0f4205c3f0e80e4a
AndreeBermudez <andreebermudez1408@gmail.com>
2025-06-17 14:29:08 -0500

    Ejecutable

---
+++ app/(tabs)/explore_new_working.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function ExploreNewWorking() {
+	return (
+		<View>
+			<Text>Explore New Working</Text>
+		</View>
+	);
+}
